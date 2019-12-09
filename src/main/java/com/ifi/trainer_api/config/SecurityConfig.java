8db06f1d55f0d0b3188f82fa6852ff1f4a226e43
Verquerre Clara <verquerre@b11p3.fil.univ-lille1.fr>
2019-12-09 10:36:59 +0100

    move securityConfig in package

--- src/main/java/com/ifi/trainer_api/config/SecurityConfig.java
+++ src/main/java/com/ifi/trainer_api/config/SecurityConfig.java
@@ -1,3 +1,5 @@
+package com.ifi.trainer_api.config;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
